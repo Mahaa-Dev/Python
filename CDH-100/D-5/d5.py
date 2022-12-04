be184e4c7cdaad399b51a45df5265255f050a65f
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2022-12-04 19:54:48 +0545

    day-5

---
+++ CDH-100/D-5/d5.py
@@ -0,0 +1,20 @@
+print("we are going to learn about comments, escape sequence and print statement")
+
+# To begin with comment, we can comments by using 3 ways. First is you can use hash or pound symbol at the very beginning of statement
+'''
+for multiline comments, you can comment in this way
+'''
+"""
+you can also use double quotation for comments
+"""
+# to print in next line you have to use \n otherwise you will get syntax error like string literal
+
+print("hello what's up \nand keep it up")
+
+# if we have to put quotiation in print statement then you have to use \" "\
+
+print("this a kid from \" 90's \" and come to nepal in 20's")
+
+# print statement
+print("hey", 5, 9, sep="!", end="007\n")
+print("Shiv")
