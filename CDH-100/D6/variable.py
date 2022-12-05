99fd5a9e9e9ab87156e0c02636ba8a084570ddc5
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2022-12-05 20:16:04 +0545

    day 6

---
+++ CDH-100/D6/variable.py
@@ -0,0 +1,24 @@
+a = 1
+print(a)
+b = "shah"
+print(b)
+print("the type of a is:", type(a))
+print("the type of b is:", type(b))
+
+# built in datatypes are int float complex
+
+c = complex(8, 3)
+print(c)
+
+# list is collection of datatypes
+list1 = [8, 2.1, [-4, 2], ["Hari", "shiv"]]
+print(list1)
+
+# tuples are just like list but it is immutable
+tuple1 = (("pg", "sg"), ("ms", "ds"))
+print(tuple1)
+
+# dictionary is combination of key and value
+
+dict1 = {"name": "shiv", "age": 20}
+print(dict1)
