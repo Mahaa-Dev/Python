d5188a562b88cf402137152626a5fc74ed92a391
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2022-12-11 21:56:20 +0545

    Day7 added arithmetic operators

---
+++ CDH-100/d7/d7.py
@@ -0,0 +1,14 @@
+a = 8
+b = 6
+c = a+b
+print("Sum of a and b is:", c)
+d = a - b
+print("difference of a and b is:", d)
+
+e = a/b
+
+print("division of a by b is:", e)
+
+f = a//b
+
+print("Floor value is:", f)
