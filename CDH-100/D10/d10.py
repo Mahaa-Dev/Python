985a6771ed9141f151de2dc7fdff64d453a2a1e9
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2022-12-12 22:29:55 +0545

    userinput day10

---
+++ CDH-100/D10/d10.py
@@ -0,0 +1,13 @@
+# a = input()
+# print("My name is", a)
+
+# b = input("Enter your name:")
+# print("My name is:", b)
+
+# x = input("Enter first number:")
+# y = input("Enter second number:")
+# print(x + y) # this will print the both by concatening it as input function ony takes string so we need to type cast it
+
+x = input("Enter first number:")
+y = input("Enter second number:")
+print(int(x) + int(y))
