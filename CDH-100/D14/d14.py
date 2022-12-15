52a59732363b2b2d687b222e65c775dea43f15cd
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2022-12-15 22:26:20 +0545

    If-Else condition

---
+++ CDH-100/D14/d14.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your age:"))
+print("Your age is ", a)
+
+if (a > 20):
+    print("You can drink")
+else:
+    print("You cannot drink")
---
+++ CDH-100/D14/if-else.py
@@ -0,0 +1,8 @@
+apple = 200
+banana = 200
+
+if (apple >= 200):
+    print("Alexa, add 1kg apples to cart")
+
+else:
+    print("Alexa, do not add to cart")
