37f57975afb8272cc7f4c36e34c13292abdcbb7e
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2022-12-16 21:56:26 +0545

    FOR LOOP

---
+++ CDH-100/D17/d17.py
@@ -0,0 +1,18 @@
+# name = "dikshendra"
+# for i in name:
+#     print(i, end=', ')
+
+# colors = ["Red", "Blue", "Green", "Yellow"]
+# for x in colors:
+#     print(x)
+#     for i in x:
+#         print(i)
+
+# for i in range(5):
+#     print(i)
+
+# for x in range(1, 20000):
+#     print(x)
+
+for j in range(1, 10, 2):
+    print(j)
