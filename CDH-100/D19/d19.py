fead7273cd0cc843a892dfe23601c7ac79f1ad24
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2022-12-17 21:47:34 +0545

    BREak statement

---
+++ CDH-100/D19/d19.py
@@ -0,0 +1,11 @@
+# for i in range(12):
+#     if (i == 10):
+#         break
+#     print("5 X", i+1, "=", 5*(i+1))
+# print("out of loop")
+
+for i in range(12):
+    if (i == 10):
+        continue
+    print("5 X", i, "=", 5*i)
+print("out of loop")
